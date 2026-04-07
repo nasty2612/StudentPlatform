c73e247a09585456d0c98bc1b3641d20eb3889a7
nasty2612 <189107026+nasty2612@users.noreply.github.com>
2026-04-08 00:28:50 +0300

    Создаем класс инфракстуктуры для дальнейшего удобного использования

---
+++ StudentPlatform/Infrastructure/AppConfig.cs
@@ -0,0 +1,19 @@
+﻿namespace StudentPlatform.Infrastructure
+{
+    public class AppConfig
+    {
+        public TinyMCE TinyMCE { get; set; } = new TinyMCE();
+        public StudentPlatform StudntPlatform { get; set; } = new StudentPlatform();
+    }
+
+    public class TinyMCE
+    {
+        public string? APIKey { get; set; }
+    }
+
+    public class StudentPlatform
+    {
+        public string? Name { get; set; }
+        public string? Help { get; set; }
+    }
+}
