85cf847ad8f5c36019bf3409db0ec4dab4d17bcb
nasty2612 <189107026+nasty2612@users.noreply.github.com>
2026-04-08 13:22:11 +0300

    Почти законечнный шаблон - подключеный контроллеры, сделана главная страница

--- StudentPlatform/Program.cs
+++ StudentPlatform/Program.cs
@@ -1,11 +1,23 @@
+using StudentPlatform.Infrastructure;
+
 namespace StudentPlatform
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
-            var app = builder.Build();
+            WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+            WebApplication app = builder.Build();
+
+            // Подключаем в конфигурацию файл appsettings.json
+            IConfigurationBuilder configBuild = new ConfigurationBuilder()
+                .SetBasePath(builder.Environment.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddEnvironmentVariables();
+
+            // Оборачиваем секцию Project в обьектную форму для удобства
+            IConfiguration configuration = configBuild.Build();
+            AppConfig config = configuration.GetSection("Project").Get<AppConfig>()!;
 
             app.MapGet("/", () => "Hello World!");
 
