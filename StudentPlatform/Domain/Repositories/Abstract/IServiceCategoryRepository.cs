a99b854a38a554d90ddb1d31e952def193cb3c5d
nasty2612 <189107026+nasty2612@users.noreply.github.com>
2026-04-20 17:55:14 +0300

    Репозитории для функций при работе с постами

---
+++ StudentPlatform/Domain/Repositories/Abstract/IServiceCategoryRepository.cs
@@ -0,0 +1,11 @@
+﻿using StudentPlatform.Domain.Entities;
+
+namespace StudentPlatform.Domain.Repositories.Abstract
+{
+    public interface IServiceCategoryRepository
+    {
+        Task<IEnumerable<ServiceCategory>> GetServiceCategoriesAsync();
+        Task<ServiceCategory?> GetServiceCategoryByIdAsync(int id);
+        Task SaveServiceCategoryAsync(ServiceCategory entity);
+        Task DeleteServiceCategoryAsync(int id);
+}
---
+++ StudentPlatform/Domain/Repositories/EntityFramework/EFServiceCategoriesRepository.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentPlatform.Domain.Entities;
+using StudentPlatform.Domain.Repositories.Abstract;
+
+namespace StudentPlatform.Domain.Repositories.EntityFramework
+{
+    public class EFServiceCategoriesRepository : IServiceCategoryRepository
+    {
+        private readonly AppDbContext _context;
+        public EFServiceCategoriesRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable<ServiceCategory>> GetServiceCategoriesAsync()
+        {
+            return await _context.ServiceCategories.Include(x=>x.Services).ToListAsync();
+        }
+        public async Task<ServiceCategory?> GetServiceCategoryByIdAsync(int id)
+        {
+            return await _context.ServiceCategories.Include(x => x.Services).FirstOrDefaultAsync(x => x.Id == id);
+        }
+    }
